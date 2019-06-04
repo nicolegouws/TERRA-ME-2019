811f045d0615a259ba48d65c87a883fb77458622
Nicole Gouws <NicoleGouws09@gmail.com>
2019-06-04 06:15:00 +0200

    new cards

--- public/src/search-results.js
+++ public/src/search-results.js
@@ -5,78 +5,54 @@ class SearchResults extends HTMLElement{
     connectedCallback(){
         this.innerHTML = `
         <ion-content>
+        <ion-app>
         <h2>
-    Plant search results
+    Confirm your plant
   </h2>
-<hr style="border: 1px solid;" />
-<ion-searchbar animated></ion-searchbar>
-<h3 style="text-align:left ; margin:24px; color:black">Search results for: “Cactus”</h3>
-<hr style="border: 1px solid;" />
-
-<div>
-    <img id="searchResulImages" src="assets/agave.jpg">
-    <div class="searchResultText">
-        Agave
-        <ion-fab class="readMore"></ion-fab>
-                <ion-icon name="more"></ion-icon>
-            </ion-fab-button>
-            <ion-fab-list side="top">
-            <span><ion-icon name="more"></ion-icon></span>
-                <div class="dropdown-content">
-                <div class="dropdown">
-                    <div class="centerlist">
-                     <div class="rose">
-                     <ul>
-                     <li> <ion-icon class="rose" name="rose"></ion-icon>Add to my garden</li>
-                     <li> <ion-icon class="rose" name="rose"></ion-icon>Add to my garden</ion-icon></li>
-                     </ul>
-                <div/>
-             </div>
-            </div>
-
-
-            <div>
-            <img id="searchResulImages" src="assets/Aloe1.jpg">
-            <div class="searchResultText">
-                Aloe
-                <ion-fab class="readMore"></ion-fab>
-                        <ion-icon name="more"></ion-icon>
-                    </ion-fab-button>
-                    <ion-fab-list side="top">
-                    <span><ion-icon name="more"></ion-icon></span>
-                        <div class="dropdown-content">
-                        <div class="dropdown">
-                            <div class="centerlist">
-                             <div class="rose">
-                             <ul>
-                             <li> <ion-icon class="rose" name="rose"></ion-icon>Add to my garden</li>
-                             <li> <ion-icon class="rose" name="rose"></ion-icon>Add to my garden</ion-icon></li>
-                             </ul>
-                        <div/>
-                     </div>
-                    </div> 
-
+        <hr style="border: 1px solid;" />
+        <h3 style="text-align:left ; margin:24px; color:black">We think we found a match. Please select a plant to confirm or ask us to try again.</h3>
+        <hr style="border: 1px solid;" />
+        <ion-grid>
+            <ion-row align-items-start>
+                <ion-col>
                     <div>
-                    <img id="searchResulImages" src="assets/burrostailbasket-1200x1200.jpg">
-                    <div class="searchResultText">
-                        Burros Tail
-                        <ion-fab class="readMore"></ion-fab>
-                                <ion-icon name="more"></ion-icon>
-                            </ion-fab-button>
-                            <ion-fab-list side="top">
-                            <span><ion-icon name="more"></ion-icon></span>
-                                <div class="dropdown-content">
-                                <div class="dropdown">
-                                    <div class="centerlist">
-                                     <div class="rose">
-                                     <ul>
-                                     <li> <ion-icon class="rose" name="rose"></ion-icon>Add to my garden</li>
-                                     <li> <ion-icon class="rose" name="rose"></ion-icon>Add to my garden</ion-icon></li>
-                                     </ul>
-                                <div/>
-                             </div>
-                            </div> 
+                        <img id="cactus" src="/Users/nicolegouws/TERRA-ME-2019/public/assets/Cactus_thumbnail.png">
+                        <div class="searchResultText">
+                            Cactus
+                            <div/>
+                        </div>
+                    </div>
+                </ion-col>
+                <ion-col>
+                    <div>
+                        <img id="agave" src="assets/Agave_thumbnail.png">
+                        <div class="searchResultText">
+                            Agave
+                            <div/>
+                        </div>
+                    </div>
+                </ion-col>
+            </ion-row>
+            <ion-row align-items-start>
+                <ion-col>
+                    <div>
+                        <img id="Aloe" src="/Users/nicolegouws/TERRA-ME-2019/public/assets/Aloe_Thumbnail.png">
+                        <div class="searchResultText">
+                            Aloe
+                            <div/>
+                        </div>
+                    </div>
+                </ion-col>
+                <ion-col>
+                    <div class="fixed">
+                        <ion-button>Add a plant </ion-button>
+                    </div>
 
+                </ion-col>
+            </ion-row>
+
+        </ion-grid>
+    </ion-app>
         </ion-content>
         <terra-footer></terra-footer>
                     `;   
